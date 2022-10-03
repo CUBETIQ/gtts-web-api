2037e145576990966dca9f714654590bbd814d3a
Sambo Chea <sombochea@cubetiqs.com>
2022-10-03 18:01:10 +0700

    Add env for expose

--- README.md
+++ README.md
@@ -3,9 +3,16 @@
 - gTTS
 
 ### Usages
+- For Khmer Language
 ```text
-#
+https://gtts-web-api.heroku.ctdn.dev?text=សួស្ដី&lang=km
 ```
+
+- For English Language
+```text
+https://gtts-web-api.heroku.ctdn.dev?text=hello&lang=en
+```
+
 
 ### Contributors
 - Sambo Chea <sombochea@cubetiqs.com>
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import environ
 from flask import Flask, request, Response
 from gtts import gTTS
 from io import BytesIO
@@ -16,5 +17,9 @@ def index():
     tts = gTTS(text=text, lang=lang)
     mp3_fp = BytesIO()
     tts.write_to_fp(mp3_fp)
-    
+
     return Response(mp3_fp.getvalue(), mimetype='audio/mpeg')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=environ.get('PORT', 5000))
