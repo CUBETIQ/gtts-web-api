8cc44bcb93fc8183edde65f3a4af0f784724e577
Sambo Chea <sombochea@cubetiqs.com>
2022-10-03 18:14:43 +0700

    Add host for expose

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=environ.get('PORT', 5000), debug=False)
+    app.run(host=environ.get('HOST', '0.0.0.0'), port=environ.get('PORT', 5000), debug=False)
