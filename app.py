6ba858061de30d71b576db3ac2f77b3ed59ad22e
Sambo Chea <sombochea@cubetiqs.com>
2022-10-03 18:22:49 +0700

    Add support args and form

--- app.py
+++ app.py
@@ -6,20 +6,32 @@ from io import BytesIO
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    text = request.args.get('text')
-    lang = request.args.get('lang') or 'km'
+    text = request.args.get('text') or request.form.get('text')
+    lang = request.args.get('lang') or request.form.get('lang') or 'km'
+    tld = request.args.get('tld') or request.form.get('tld') or 'com'
+    slow = request.args.get('slow') or request.form.get('slow') or False
+    dl = request.args.get('dl') or request.form.get('dl') or False
 
     if not text:
-        return Response('No text specified', status=400)
+        return Response({
+            'status': 'error',
+            'message': 'text is required'
+        }, status=400)
 
-    tts = gTTS(text=text, lang=lang)
+    tts = gTTS(text=text, lang=lang, tld=tld, slow=slow)
     mp3_fp = BytesIO()
     tts.write_to_fp(mp3_fp)
+
+    if dl:
+        return Response(mp3_fp.getvalue(), mimetype='audio/mpeg', headers={
+            'Content-Disposition': 'attachment; filename="audio.mp3"'
+        })
 
     return Response(mp3_fp.getvalue(), mimetype='audio/mpeg')
 
 
 if __name__ == '__main__':
-    app.run(host=environ.get('HOST', '0.0.0.0'), port=environ.get('PORT', 5000), debug=False)
+    app.run(host=environ.get('HOST', '0.0.0.0'),
+            port=environ.get('PORT', 5000), debug=False)
