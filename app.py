5514b42a255f06b5fcf51d77ce69880704e29f27
Sambo Chea <sombochea@cubetiqs.com>
2022-10-03 17:33:45 +0700

    init project

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, Response
+from gtts import gTTS
+from io import BytesIO
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    text = request.args.get('text')
+
+    if not text:
+        return Response('No text specified', status=400)
+
+    tts = gTTS(text=text, lang='km')
+    mp3_fp = BytesIO()
+    tts.write_to_fp(mp3_fp)
+    
+    return Response(mp3_fp.getvalue(), mimetype='audio/mpeg')
